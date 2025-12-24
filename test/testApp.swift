9518a082605c6c508efd6042dbd7112919e4133a
mzelzoghbi <m.zakaria.elzoghbi@gmail.com>
2025-12-24 09:25:17 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Zak on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
